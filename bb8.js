cad88c418b8cbbc27ab7876ea0e21d60f4e24275
johannahenning <hennjoha@gmx.de>
2018-01-04 13:41:02 +0100

    square function

--- bb8.js
+++ bb8.js
@@ -4,70 +4,82 @@ var Cylon = require('cylon');
 var keypress = require("keypress");
 
 
-    Cylon.robot({
-      connections: {
-        bluetooth: { adaptor: 'central', uuid: 'ef5b943330b9', module: 'cylon-ble'}
-      },
+Cylon.robot({
+    connections: {
+        bluetooth: {adaptor: 'central', uuid: 'ef5b943330b9', module: 'cylon-ble'}
+    },
 
-      devices: {
-        bb8: { driver: 'bb8', module: 'cylon-sphero-ble'}
-      },
+    devices: {
+        bb8: {driver: 'bb8', module: 'cylon-sphero-ble'}
+    },
 
-      work: function(my) {
+    work: function (my) {
 
         function handle(ch, key) {
 
-          if (key.ctrl && key.name === "c") {
-              process.stdin.pause();
-              process.exit();
-          }
+            if (key.ctrl && key.name === "c") {
+                process.stdin.pause();
+                process.exit();
+            }
 
-          switch (key.name) {
+            switch (key.name) {
 
-            case "c":
-            var myColor = setInterval(function () {
-              my.bb8.randomColor();
-            }, 1000);
-              // my.bb8.randomColor();
-            break;
+                case "c":
+                    var myColor = setInterval(function () {
+                        my.bb8.randomColor();
+                    }, 1000);
+                    // my.bb8.randomColor();
+                    break;
 
-            case "k":
-              my.bb8.randomColor();
-            break;
+                case "q":
+                    var counter = 0;
+                    setInterval(function () {
+                        console.log(counter);
+                        counter = counter + 90;
+                        my.bb8.roll(100, counter);
+                        if (counter === 180) {
+                            my.bb8.stop();
+                        }
+                    }, 1000);
+                    break;
 
-            case "w":
-              my.bb8.randomColor();
-              my.bb8.roll(100, 90);
-            break;
+                case "k":
+                    my.bb8.randomColor();
+                    break;
 
-            case "d":
-              my.bb8.randomColor();
-              my.bb8.roll(100, 180);
-            break;
+                case "w":
+                    my.bb8.randomColor();
+                    my.bb8.roll(100, 90);
+                    break;
 
-            case "s":
-              my.bb8.randomColor();
-              my.bb8.roll(100, 270);
-            break;
+                case "d":
+                    my.bb8.randomColor();
+                    my.bb8.roll(100, 180);
+                    break;
 
-            case "a":
-              my.bb8.randomColor();
-              my.bb8.roll(100, 0);
-            break;
+                case "s":
+                    my.bb8.randomColor();
+                    my.bb8.roll(100, 270);
+                    break;
 
-            case "space":
-              my.bb8.stop();
-            break;
+                case "a":
+                    my.bb8.randomColor();
+                    my.bb8.roll(100, 0);
+                    break;
+
+                case "space":
+                    my.bb8.stop();
+                    break;
             }
             //
             // my.bb8.detectCollisions();
             // my.bb8.stopOnDisconnect();
-          }
+        }
 
-          keypress(process.stdin);
-          process.stdin.on("keypress", handle);
+        keypress(process.stdin);
+        process.stdin.on("keypress", handle);
 
-          process.stdin.setRawMode(true);
-          process.stdin.resume();
-      }
-    }).start();
+        process.stdin.setRawMode(true);
+        process.stdin.resume();
+    }
+}).start();
