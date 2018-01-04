a9fb72cd66476932299e5d26094b513ab0658670
johannahenning <hennjoha@gmx.de>
2018-01-04 15:41:07 +0100

    test1!

--- bb8.js
+++ bb8.js
@@ -102,12 +102,12 @@ Cylon.robot({
                     console.log(counter);
                     my.bb8.roll(10, counter);
                     counter = counter + 90;
-                    if (counter === 360) {
+                    if (counter === 450) {
                         console.log("STOP!");
                         my.bb8.stop();
                         clearInterval(interval);
                     }
-                }, 3000);
+                }, 8000);
                 break;
           }
         }
