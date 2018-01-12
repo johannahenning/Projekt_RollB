6c2af4590e0d5e479e772d852199017f8fdc9abe
johannahenning <hennjoha@gmx.de>
2018-01-12 10:32:31 +0100

    Kreis

--- bb8.js
+++ bb8.js
@@ -91,9 +91,11 @@ Cylon.robot({
                 console.log("ENDE!");
             break;
 
-
+              case "m":
+                  var audio = new Audio('test.mp3');
+                  audio.play();
+                  break;
           }
-
 
         }
 
