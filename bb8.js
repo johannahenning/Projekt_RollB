50f58af38d54a7ed4e270b2e4dde13ef1e2947e6
johannahenning <hennjoha@gmx.de>
2018-02-02 14:27:54 +0100

    Sound

--- bb8.js
+++ bb8.js
@@ -17,177 +17,108 @@ Cylon.robot({
     work: function (my) {
 
         function handle(ch, key) {
-        }
 
             if (key.ctrl && key.name === "c") {
                 process.stdin.pause();
                 process.exit();
             }
 
-            key.name.switch(key.name)
-            {
+            switch (key.name) {
 
-            case
-                "q"
-            :
-                var myColor = setInterval(function () {
+                case "c":
+                    var myColor = setInterval(function () {
+                        my.bb8.randomColor();
+                    }, 1000);
+                    break;
+
+                case "k":
                     my.bb8.randomColor();
-                },  1000);
-                break;
+                    break;
 
-            case
-                "w"
-            :
-                my.bb8.setBackLed();
-                break;
+                case "keyup":
+                    my.bb8.randomColor();
+                    my.bb8.roll(100, 90);
+                    break;
 
-            case
-                "k"
-            :
-                my.bb8.randomColor();
-                break;
+                case "d":
+                    my.bb8.randomColor();
+                    my.bb8.roll(100, 180);
+                    break;
 
-            case
-                "keyup"
-            :
-                my.bb8.randomColor();
-                my.bb8.roll(100, 90);
-                break;
+                case "s":
+                    my.bb8.randomColor();
+                    my.bb8.roll(100, 270);
+                    break;
 
-            case
-                "keyright"
-            :
-                my.bb8.randomColor();
-                my.bb8.roll(100, 180);
-                break;
+                case "a":
+                    my.bb8.randomColor();
+                    my.bb8.roll(100, 0);
+                    break;
 
-            case
-                "keydown"
-            :
-                my.bb8.randomColor();
-                my.bb8.roll(100, 270);
-                break;
+                case "w":
+                    my.bb8.setBackLed();
+                    break;
 
-            case
-                "keyleft"
-            :
-                my.bb8.randomColor();
-                my.bb8.roll(100, 0);
-                break;
+                case "space":
+                    my.bb8.stop();
+                    break;
 
-            case
-                "space"
-            :
-                my.bb8.stop();
-                break;
+                case "q":
+                    var dir = 0;
+                    var interval = setInterval(function(){
+                        console.log("drive to direction: " + dir);
+                        my.bb8.roll(30, dir);
+                        dir = dir + 90;
+                        if (dir === 450) {
+                            console.log("STOP!");
+                            my.bb8.stop();
+                            clearInterval(interval);
+                        }
+                    }, 3000);
+                    break;
 
-            case
-                "q"
-            :
-                var dir = 0;
-                var interval = setInterval(function () {
-                    console.log("drive to direction: " + dir);
-                    my.bb8.roll(30, dir);
-                    dir = dir + 90;
-                    if (dir === 450) {
-                        console.log("STOP!");
-                        my.bb8.stop();
-                        clearInterval(interval);
-                    }
-                }, 3000);
-                break;
+                case "o":
 
-            case
-                "o"
-            :
+                    var count = 0;
+                    var dir = 0;
+                    var interval = setInterval(function () {
+                        console.log("drive to direction: " + dir);
+                        my.bb8.roll(30, dir);
+                        dir = dir + 5;
+                        if (dir >= 365) {
+                            dir = 0;
+                            console.log("Wieder auf 0");
+                            count++;
+                        }
+                        if(count > 4) {
+                            clearInterval(interval);
+                            my.bb8.stop();
+                            console.log("ENDE!");
+                        }
+                    }, 100);
 
-                var count = 0;
-                var dir = 0;
-                var interval = setInterval(function () {
-                    console.log("drive to direction: " + dir);
-                    my.bb8.roll(30, dir);
-                    dir = dir + 5;
-                    if (dir >= 365) {
-                        dir = 0;
-                        console.log("Wieder auf 0");
-                        count++;
-                    }
-                    if (count > 4) {
-                        clearInterval(interval);
-                        my.bb8.stop();
-                        console.log("ENDE!");
-                    }
-                }, 100);
+                    break;
 
-                break;
+                case ("y"):
 
-            case
-                ("y")
-            :
-                var player = new SoundPlayer();
-                player.sound('1.mp3', function () {
-                });
-                break;
+                    var player=new SoundPlayer();
 
+                    player.sound('1.mp3', function(){
 
-            case
-                ("x")
-            :
-                var player = new SoundPlayer();
-                player.sound('2.mp3', function () {
-                });
-                break;
+                        // player.sound('test.mp3');
 
+                    });
 
-            case
-                ("c")
-            :
-                var player = new SoundPlayer();
-                player.sound('3.mp3', function () {
-                });
-                break;
-
-
-            case
-                ("v")
-            :
-                var player = new SoundPlayer();
-                player.sound('4.mp3', function () {
-                });
-                break;
-
-
-            case
-                ("b")
-            :
-                var player = new SoundPlayer();
-                player.sound('5.mp3', function () {
-                });
-                break;
-
-
-            case
-                ("n")
-            :
-                var player = new SoundPlayer();
-                player.sound('6.mp3', function () {
-                });
-                break;
-
-            case
-                ("m")
-            :
-                var player = new SoundPlayer();
-                player.sound('7.mp3', function () {
-                });
-                break;
+                    break;
             }
 
-
-            keypress(process.stdin);
-            process.stdin.on("keypress", handle);
-
-            process.stdin.setRawMode(true);
-            process.stdin.resume();
         }
-    }).start();
+
+
+        keypress(process.stdin);
+        process.stdin.on("keypress", handle);
+
+        process.stdin.setRawMode(true);
+        process.stdin.resume();
+    }
+}).start();
