7eb25bd7d180691f4273d049cc7e7f76f3be9795
johannahenning <hennjoha@gmx.de>
2018-07-23 21:40:25 +0200

    23/02

--- rollB.js
+++ rollB.js
@@ -452,11 +452,7 @@ Cylon.robot({
             bestaetigungsFarbe(function () {
                 player.sound('soundfiles/Klo/aufToilette.mp3', function () { //"oh ich merke ich muss aufs klo..."
                     driveToKoord(blueTargetX, blueTargetY, function () {
-                        player.sound('soundfiles/Klo/pissAndFlush.mp3', function () {
-                            driveToKoord(redTargetX, redTargetY, function () {
-                                player.sound('soundfiles/Klo/dasWarErleichternd.mp3');
-                            });
-                        });
+                        player.sound('soundfiles/Klo/pissAndFlush.mp3');
                     });
                 })
             });
